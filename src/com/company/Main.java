9bba012c49fe71a825056799852e53184e41cd38
Tejashegde1595 <Tejashegde.1595@gmail.com>
2021-09-06 18:44:39 +0530

    Commit With Ignore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	        System.out.println("Git Versioning From IntelliJIdea");
+	        System.out.println("Git Versioning From IntelliJIdeas");
     }
 }
