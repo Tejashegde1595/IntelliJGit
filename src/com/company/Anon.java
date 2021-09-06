d70b02d3a4d7eda8dd2cf38727939a6ad8ceb737
Tejashegde1595 <Tejashegde.1595@gmail.com>
2021-09-06 19:04:44 +0530

    Made Change to Anon

--- src/com/company/Anon.java
+++ src/com/company/Anon.java
@@ -5,4 +5,8 @@ public class Anon {
     public void print(){
         System.out.println("Anon class print successful");
     }
+
+    public void talk(){
+        System.out.println("Talking");
+    }
 }
